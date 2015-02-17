9c0e73627635f0f95eb4ad1c0624a80fc0e6cfce
tommiCZE <tomas.knytl@gmail.com>
2015-02-17 11:55:38 +0100

    update pom.xml

--- Carservice-app/src/main/java/cz/tommi/carservices/controller/HomeController.java
+++ Carservice-app/src/main/java/cz/tommi/carservices/controller/HomeController.java
@@ -4,14 +4,13 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -19,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LogManager.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
--- Carservice-app/src/main/webapp/WEB-INF/views/greeting.jsp
+++ Carservice-app/src/main/webapp/WEB-INF/views/greeting.jsp
@@ -3,11 +3,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>greetingString</title>
-<h1>${greetingString}</h1>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>greetingString</title>
 </head>
 <body>
-
+	<h1>${greetingString}</h1>
 </body>
 </html>
